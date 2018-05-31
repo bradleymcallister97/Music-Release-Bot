b6878168cde5141c27271a91731ac5d3e378fce9
bradleymcallister97 <brad.mcallister@hotmail.com>
2018-05-31 16:29:29 -0400

    add cron job every day at 9

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cron = require('node-cron');
 const config = require('./config');
 
-if (!config.facebook.token || !config.spotify.token){
+if (!config.facebook.token || !config.spotify.token) {
     throw new Error('Not All Tokens have been initialized');
 }
 
@@ -18,3 +19,8 @@ app.get('/', require('./controllers/verification'));
 app.post('/', require('./controllers/messageWebhook'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
+
+const job = require('./helpers/checkNewAlbums');
+cron.schedule('0 9 * * *', () => {
+    job();
+});
