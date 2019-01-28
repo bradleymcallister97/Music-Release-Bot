8a676d58d1138529244cf993f971751a45dbe6c7
bradleymcallister97 <brad.mcallister@hotmail.com>
2019-01-28 11:12:45 -0500

    update promise chain in cron job

--- helpers/checkNewAlbums.js
+++ helpers/checkNewAlbums.js
@@ -20,16 +20,20 @@ const run = () => {
     today.setUTCHours(0,0,0,0);
 
     return getAllArtitsts().then((artists) => {
-        artists.forEach((artist) => {
-            searchAlbums(artist.id).then((albums) => {
-                albums.forEach((a) => {
-                    if (a.dateReleased.getTime() === today.getTime()) {
-                        console.log(artist.id + ' released ' + a.name + ' on ' + today);
-                        messageUsersForArtist(artist.id, artist.name, a.name);
-                    }
-                });
-            });
-        });
+        const promises = [];
+        for (artist in artists) {
+            promises.push( 
+                searchAlbums(artist.id).then((albums) => {
+                    for (a in albums) {
+                        if (a.dateReleased.getTime() === today.getTime()) {
+                            console.log(artist.id + ' released ' + a.name + ' on ' + today);
+                            return messageUsersForArtist(artist.id, artist.name, a.name);
+                        }
+                    };
+                })
+            );
+        };
+        return Promise.all(promises);
     });
 }
 
